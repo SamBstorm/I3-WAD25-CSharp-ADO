82925c7a25e34007afa6fefe16c3fb167b2eade6
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-19 10:20:00 +0100

    Add Demo-Ado : ConnectionString + Nuget Packages

---
+++ Demo-Ado-01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_Ado_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Demo-DB;Integrated Security=True";
+        }
+    }
+}
