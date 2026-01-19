7bacac680f5639c89a1b5479fcbd35409062530b
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-19 11:05:56 +0100

    Update Demo-Ado : IDisposable

--- Demo-Ado-01/Program.cs
+++ Demo-Ado-01/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Demo_Ado_01
+﻿using Microsoft.Data.SqlClient;
+
+namespace Demo_Ado_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Demo-DB;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                Console.WriteLine(connection.State);
+
+                connection.Open();
+
+                Console.WriteLine(connection.State);
+
+                connection.Close();
+
+                Console.WriteLine(connection.State);
+            }
         }
     }
 }
