16b91d0b961340022be8505df9f8f31761c46700
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-19 14:27:27 +0100

    Update Demo-Ado : SqlDataAdpater (Disconnected Mode)

--- Demo-Ado-01/Program.cs
+++ Demo-Ado-01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace Demo_Ado_01
 {
@@ -8,6 +9,7 @@ namespace Demo_Ado_01
         {
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Demo-DB;Integrated Security=True";
 
+            /* Mode connecté : ExecuteScalar (une seule valeur)
             int nbProduct;
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -25,24 +27,61 @@ namespace Demo_Ado_01
             }
 
             Console.WriteLine($"Dans ma DB sont enregistrés {nbProduct} produit(s).");
+            */
+            /* Mode connecté : ExecuteReader (Plusieurs lignes/colonnes)
+            List<Product> products = new List<Product>();
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 using (SqlCommand command = connection.CreateCommand())
                 {
-                    command.CommandText = "SELECT [Name], [Description] FROM [Product]";
+                    command.CommandText = "SELECT [ProductId], [Name], [Description], [CreationDate] FROM [Product]";
                     connection.Open();
 
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Console.WriteLine($"\t- {reader["Name"]}\n\t\t{reader["Description"]}");
+                            products.Add(new Product()
+                            {
+                                ProductId = (int)reader[nameof(Product.ProductId)],
+                                Name = (string)reader[nameof(Product.Name)],
+                                Description = (reader[nameof(Product.Description)] is DBNull) ? null : (string?)reader[nameof(Product.Description)],
+                                CreationDate = (DateTime)reader[nameof(Product.CreationDate)]
+                            });
                         }
                     }
 
                     connection.Close();
                 }
+            }
+
+            foreach (var prod in products)
+            {
+                Console.WriteLine($"\t- {prod.Name}\n\t\t{prod.Description} - {prod.CreationDate.ToShortDateString()}");
+            }*/
+
+            /* Mode Déconnecté : SqlDataAdapter (Dans des DataTable)*/
+
+            DataTable productTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [ProductId], [Name], [Description], [CreationDate] FROM [Product]";
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        adapter.SelectCommand = command;
+                        adapter.Fill(productTable);
+                    }
+                }
+            }
+
+            foreach (DataRow row in productTable.Rows)
+            {
+                Console.WriteLine($"\t- {row["Name"]}\n\t\t{row["Description"]} - {((DateTime)row["CreationDate"]).ToShortDateString()}");
             }
         }
     }
