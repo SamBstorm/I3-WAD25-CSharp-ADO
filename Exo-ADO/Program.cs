33f5b1cdd305761430e9b694faca9a04965443dd
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-19 11:31:07 +0100

    Add Correction : Exo-ADO : SqlConnection

---
+++ Exo-ADO/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Exo_ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExoWAD-ADO;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine(connection.State);
+                connection.Open();
+                Console.WriteLine(connection.State);
+                connection.Close();
+                Console.WriteLine(connection.State);
+            }
+        }
+    }
+}
