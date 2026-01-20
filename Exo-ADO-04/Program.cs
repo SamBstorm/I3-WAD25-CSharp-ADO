f485210a903d4f4796fcade90aed2b004dacf915
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-20 16:24:32 +0100

    Add Exo-Ado-04 & Exo-Ado-05

---
+++ Exo-ADO-04/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Exo_ADO_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExoWAD-ADO;Integrated Security=True";
+
+            Student voisin = new Student()
+            {
+                FirstName = "Alexandre",
+                LastName = "Claes",
+                BirthDate = new DateTime(1987,1,1),
+                SectionID = 1010,
+                YearResult = 16
+            };
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO [Student] ([FirstName], [LastName], [BirthDate], [YearResult], [SectionID]) OUTPUT [inserted].[Id], [inserted].[Active] VALUES (@fn, @ln, @bd, @yr, @sid)";
+
+                    SqlParameter pFN = new SqlParameter() { 
+                        ParameterName = "fn",
+                        Value = voisin.FirstName
+                    };
+
+                    command.Parameters.Add(pFN);
+
+                    command.Parameters.AddWithValue("ln", voisin.LastName);
+                    command.Parameters.AddWithValue("bd", voisin.BirthDate);
+                    command.Parameters.AddWithValue("yr", voisin.YearResult);
+                    command.Parameters.AddWithValue("sid", voisin.SectionID);
+
+                    try
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                voisin.Id = (int)reader[nameof(Student.Id)];
+                                voisin.Active = (bool)reader[nameof(Student.Active)];
+                            }
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            Console.WriteLine($"L'étudiant {voisin.FirstName} {voisin.LastName} a pour identifiant {voisin.Id}");
+        }
+    }
+}
---
+++ Exo-ADO-05/Program.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Exo_ADO_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExoWAD-ADO;Integrated Security=True";
+
+            Student moi = new Student() { 
+                FirstName = "Samuel",
+                LastName = "Legrain"
+            };
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT [Id], [BirthDate], [YearResult], [SectionID], [Active] FROM [Student] WHERE [FirstName] = @fn AND [LastName] = @ln";
+                    command.Parameters.AddWithValue("fn", moi.FirstName);
+                    command.Parameters.AddWithValue("ln", moi.LastName);
+                    try
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                moi.Id = (int)reader[nameof(Student.Id)];
+                                moi.BirthDate = (DateTime)reader[nameof(Student.BirthDate)];
+                                moi.SectionID = (int)reader[nameof(Student.SectionID)];
+                                moi.YearResult = (int)reader[nameof(Student.YearResult)];
+                                moi.Active = (bool)reader[nameof(Student.Active)];
+                            }
+                        }
+                    }
+                    catch(SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+
+                moi.SectionID = 1020;
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "UpdateStudent";
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("id", moi.Id);
+                    command.Parameters.AddWithValue("sectionid", moi.SectionID);
+                    command.Parameters.AddWithValue("yearresult", moi.YearResult);
+
+                    try
+                    {
+                        connection.Open();
+                        bool hasChanged = command.ExecuteNonQuery() > 0;
+                        if (hasChanged) Console.WriteLine("Mise à jour effectuée");
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+
+                int idVoisin = 1004;
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DeleteStudent";
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("id", idVoisin);
+
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+        }
+    }
+}
